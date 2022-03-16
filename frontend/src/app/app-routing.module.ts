67d51cb3bee4e0a64f30eb56da1616b64cc9ffd4
tylercchase <tylercchase@gmail.com>
2022-03-16 10:50:58 -0800

    router init

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'create', component: CreateComponent },
+{path: 'game', component: GameComponent},
+{path: '**', component: HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GameComponent } from './components/game/game.component';
+import { CreateComponent } from './components/create/create.component';
+import { HomeComponent } from './components/home/home.component';
 
+import { MatToolbarModule } from "@angular/material/toolbar";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    CreateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
