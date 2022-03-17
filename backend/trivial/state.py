ec2edbdd367452e22908a6b49e9ce1a306effc13
Rohan Weeden <reweeden@alaska.edu>
2022-03-16 17:45:01 -0800

    Fix datetime.now call

--- backend/trivial/state.py
+++ backend/trivial/state.py
@@ -30,7 +30,7 @@ class TriviaGame:
     def record_answer(self, sid: str, answer: int):
         assert self.current_question is not None
 
-        if self.trivia.config.timer and datetime() < self.current_question.start - timedelta(seconds=self.trivia.config.timer):
+        if self.trivia.config.timer and datetime.now() < self.current_question.start - timedelta(seconds=self.trivia.config.timer):
             self.answers[self.current_question.uid][sid] = answer
 
     def get_scores(self) -> dict[str, int]:
